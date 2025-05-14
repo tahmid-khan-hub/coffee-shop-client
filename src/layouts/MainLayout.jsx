94c0b2e8b87a0e24ead5e859f44de37c92c0e694
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2025-05-14 17:56:26 +0600

    first commit

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../component/Header';
+import { Outlet } from 'react-router';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Header></Header>
+            <div className='max-w-7xl mx-auto'>
+                <Outlet></Outlet>
+            </div>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import MainLayout from './layouts/MainLayout.jsx';
+import Home from './component/Home.jsx';
+import AddCoffee from './component/AddCoffee.jsx';
+import UpdateCoffee from './component/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: 'addCoffee',
+        Component: AddCoffee,
+      },
+      {
+        path: 'updateCoffee',
+        Component: UpdateCoffee,
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
+)
